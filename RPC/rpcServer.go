a4b96c323cc641c3d4a4db8ea19a9d22ad43884d
an1l4 <1995anila@gmail.com>
2022-08-30 10:52:03 +0530

    RPC-server-client

---
+++ RPC/rpcServer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"time"
+)
+
+type Args struct{}
+
+type TimeServer int64
+
+func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
+	*reply = time.Now().Unix()
+	return nil
+}
+
+func main() {
+	timeServer := new(TimeServer)
+	rpc.Register(timeServer)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+
+	if e != nil {
+		log.Fatal("listen error :", e)
+	}
+	fmt.Println("server running...")
+	http.Serve(l, nil)
+}
